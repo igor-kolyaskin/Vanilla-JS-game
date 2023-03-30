eba9224ad187c96714ea8ffcaba666bbacda0f44
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-03-30 19:18:29 +0400

    feat: add reassigning column height at initialization and applying settings

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import App from "./components/App";
 import fieldInstance from "./components/Field";
 import state from "./state/state";
 import gameInstance from "./bll/Game";
+import elements from "./state/elements";
 
 fieldInstance.init(state.fieldConfig);
+const { tileSize, numY } = state.fieldConfig;
+
 const body = document.getElementById("body");
 body.append(App());
 body.classList.add("theme-main");
-body.style.setProperty("--tile-size", `${state.fieldConfig.tileSize}rem`);
+body.style.setProperty("--tile-size", `${tileSize}rem`);
+body.style.setProperty("--column-height", `${tileSize * numY}rem`);
+
+elements.body = body;
 
 gameInstance.startNewGame();
--- src/listeners/onClickApplySettings.js
+++ src/listeners/onClickApplySettings.js
@@ -1,11 +1,17 @@
 import state from "../state/state";
 import fieldInstance from "../components/Field";
 import gameInstance from "../bll/Game";
+import elements from "../state/elements";
 
 function onClickApplySettings(event) {
   state.fieldConfig = { ...state.fieldConfigTemp };
   fieldInstance.init(state.fieldConfig);
   fieldInstance.render();
+  elements.body.style.setProperty(
+    "--column-height",
+    `${state.fieldConfig.tileSize * state.fieldConfig.numY}rem`
+  );
+
   gameInstance.startNewGame();
 }
 
