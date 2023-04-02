41b17d1783da55f4f616724f30dc6ecb0ea43eb7
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-02 21:44:30 +0400

    style: created styles for progressbar

--- src/components/sidebar/ProgressBar.js
+++ src/components/sidebar/ProgressBar.js
@@ -9,6 +9,16 @@ class ProgressBar {
     const progressBar = document.createElement("section");
     progressBar.setAttribute("id", "progressbar");
 
+    const progressBarMask = document.createElement("div");
+    progressBarMask.setAttribute("id", "progressbar-mask");
+
+    const progressBarGreen = document.createElement("div");
+    progressBarGreen.setAttribute("id", "progressbar-green");
+
+    progressBarMask.append(progressBarGreen);
+
+    progressBar.append(progressBarMask);
+
     return progressBar;
   }
 }
