8b67bcd2bc5be014cd05662613c97800db95fd6f
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-05 22:11:42 +0400

    style: add styles to bonus components

--- src/components/sidebar/Bonus.js
+++ src/components/sidebar/Bonus.js
@@ -1,7 +1,23 @@
-const Bonus = (num) => {
+const Bonus = (config) => {
+  const { id, iconUrl, tooltipText, amount } = config;
+
   const bonus = document.createElement("div");
-  bonus.setAttribute("id", `bonus-${num}`);
+  bonus.setAttribute("id", `bonus-${id}`);
   bonus.classList.add("bonus");
+
+  const bonusIcon = document.createElement("div");
+  bonusIcon.style.backgroundImage = iconUrl;
+  bonusIcon.classList.add("bonus-icon");
+
+  const bonusTooltip = document.createElement("div");
+  bonusTooltip.setAttribute("data-tooltip", `${tooltipText}`);
+  bonusTooltip.classList.add("bonus-tooltip");
+
+  const bonusAmount = document.createElement("div");
+  bonusAmount.innerText = amount;
+  bonusAmount.classList.add("bonus-amount");
+
+  bonus.append(bonusIcon, bonusTooltip, bonusAmount);
 
   return bonus;
 };
--- src/components/sidebar/Bonuses.js
+++ src/components/sidebar/Bonuses.js
@@ -5,8 +5,21 @@ const Bonuses = () => {
   const bonuses = document.createElement("section");
   bonuses.setAttribute("id", "bonuses");
 
-  const bonusOne = Bonus(1);
-  const bonusTwo = Bonus(2);
+  const confingBonus1 = {
+    id: 1,
+    iconUrl: 'url("../assets/png/color-bang-icon.png")',
+    tooltipText: "color-bang",
+    amount: 3,
+  };
+
+  const confingBonus2 = {
+    id: 2,
+    iconUrl: 'url("../assets/png/colulmn-bang-icon.png")',
+    tooltipText: "column-bang",
+    amount: 5,
+  };
+  const bonusOne = Bonus(confingBonus1);
+  const bonusTwo = Bonus(confingBonus2);
   bonusOne.addEventListener("click", () => console.log(state._fieldConfig));
 
   bonuses.append(bonusOne, bonusTwo);
