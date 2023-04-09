92d26e232aa632afefb49e623d69b37213a82e4f
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-09 09:16:54 +0400

    test: add additional argument check for func getRightWordClick, and check for Error throw in its test

--- src/test/utils/getRightWordClick.test.js
+++ src/test/utils/getRightWordClick.test.js
@@ -1,9 +1,20 @@
 import getRightWordClick from "../../utils/getRightWordClick";
 
-test("правильное склонение слова 'клик' в зависимости от числа", () => {
+test("test-de-test", () => {
+  expect(() => getRightWordClick(82)).toThrow(
+    `invalid argument (82) in func getRightWordClick`
+  );
+  expect(() => getRightWordClick(-1)).toThrow(
+    `invalid argument (-1) in func getRightWordClick`
+  );
+  expect(() => getRightWordClick(true)).toThrow(
+    `invalid argument (true) in func getRightWordClick`
+  );
+  expect(() => getRightWordClick()).toThrow(
+    `invalid argument (undefined) in func getRightWordClick`
+  );
   expect(getRightWordClick(1)).toBe("клик");
-  expect(getRightWordClick(2)).toBe("клика");
-  expect(getRightWordClick(5)).toBe("кликов");
-  expect(getRightWordClick(undefined)).toBe("кликов");
-  expect(getRightWordClick(-1)).toBe("кликов");
+  expect(getRightWordClick(3)).toBe("клика");
+  expect(getRightWordClick(12)).toBe("кликов");
+  expect(getRightWordClick(25)).toBe("кликов");
 });
--- src/utils/getRightWordClick.js
+++ src/utils/getRightWordClick.js
@@ -1,10 +1,12 @@
-const getRightWordClick = (num) => {
+function funcWithError(num) {
+  if (num > 81 || num < 1 || !Number.isInteger(num)) {
+    throw new Error(`invalid argument (${num}) in func getRightWordClick`);
+  }
   let wordClick = "кликов";
   if (+num >= 5 && +num <= 20) wordClick = "кликов";
   else if (+num % 10 === 1) wordClick = "клик";
   else if (+num % 10 >= 2 && +num % 10 <= 4) wordClick = "клика";
 
   return wordClick;
-};
-
-export default getRightWordClick;
+}
+export default funcWithError;
