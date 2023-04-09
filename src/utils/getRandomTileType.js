dca4c1a4b0e829e8c074d19dd70d86ed0ad21b5e
igor.kolyaskin <igor_kolyaskin@epam.com>
2023-04-09 22:43:33 +0400

    feat: change getRandomTileType func so that gives twin-tile with probability of 5%

--- src/utils/getRandomTileType.js
+++ src/utils/getRandomTileType.js
@@ -1,5 +1,13 @@
 const getRandomTileType = (numColors) => {
-  return Math.ceil(Math.random() * numColors);
+  // return Math.ceil(Math.random() * numColors);
+  const rnd = Math.random() * 100;
+  let result;
+  if (rnd >= 95) {
+    result = 11;
+  } else {
+    result = Math.ceil(rnd / (95 / numColors));
+  }
+  return result;
 };
 
 export default getRandomTileType;
